b64fe9d1fafd7acac7196084359ed1c89897fed8
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-28 18:31:46 +0600

    Style Card

--- src/components/models/Models.jsx
+++ src/components/models/Models.jsx
@@ -4,13 +4,13 @@ import Card from '../../ui/Card';
 const Models = ({modelPromise}) => {
     const models = use(modelPromise);
     return (
-        <div className='py-10'>
+        <div className='py-10 '>
             <div className="text-center">
                 <h2 className='text-5xl font-bold'>Chose Your AI Model</h2>
                 <p>One Subscription gives you Access to all frontier Al models</p>
             </div>
 
-            <div className="">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
                 {
                   models.map(model => ( <Card key={model.id} model={model}></Card>
                   ))
--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -3,11 +3,36 @@ import React from 'react';
 const Card = ({model}) => {
     console.log(model);
     return (
-        <div>
-            <h2>{model.title}</h2>
-            <p>{model.description}</p>
-        </div>
-    );
+     <div className="bg-white border rounded-2xl p-6 shadow-md relative">
+         {model.status === "popular" && (
+      <span className="absolute top-3 right-3 bg-blue-500 text-white text-xs px-3 py-1 rounded-full">
+        Popular
+      </span>
+    )}
+      <img
+        src={model.image}
+        alt={model.title}
+        className="w-20 mx-auto mb-4"
+      />
+
+      <h2 className="text-2xl font-bold text-center">
+        {model.title}
+      </h2>
+
+      <p className=" text-gray-600 mt-3 text-center whitespace-pre-line">
+        {model.description}
+      </p>
+
+      <p className="mt-4 text-center text-xl font-semibold text-blue-600">
+        ${model.price}/month
+      </p>
+
+       <button className="mt-5 w-full bg-blue-500 hover:bg-green-500 text-white py-2 rounded">
+      Subscribe Now
+    </button>
+   </div>
+     
+  );
 };
 
 export default Card;
