6a214e57e2aac3c0701dd52270d1f17aaf86b4d1
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-29 01:41:57 +0600

    Add total price and Use tananary to show that total and empty

--- src/components/selectCard/SelectCard.jsx
+++ src/components/selectCard/SelectCard.jsx
@@ -2,18 +2,42 @@ import React from 'react';
 import SubsCard from '../../ui/SubsCard';
 
 const SelectCard = ({ cards }) => {
-    console.log(cards)
+
+    const totalPrice = cards.reduce((sum, card) => sum + card.price, 0);
+
     return (
-        <div className=''>
+        <div>
             <h1 className='text-3xl font-bold w-10/12 mx-auto'>Your Cards</h1>
-            {
-             cards.map(card => ( <SubsCard key={card.id} card={card} ></SubsCard>
 
-             ))
-             }
+            {cards.length === 0 ? (
+           
+                <div>
+                    <p className="text-center text-2xl text-gray-500 mt-6">
+                        No cards selected
+                    </p>
+                    <p className='text-center text-gray-400'>
+                        Go to models to select a card
+                    </p>
+                </div>
+            ) : (
+                <>
+                    {
+                        cards.map(card => (
+                            <SubsCard key={card.id} card={card} />
+                        ))
+                    }
 
+                    <div className="flex justify-between items-center bg-black text-white text-xl w-10/12 mx-auto p-5 rounded-xl font-bold mt-4">
+                        <div>Total:</div>
+                        <div>${totalPrice}</div>
+                    </div>
+
+                    <button className="w-10/12 mx-auto block mt-4 bg-blue-500 hover:bg-red-500 text-white py-2 rounded-xl ">
+                        Purchase Now
+                    </button>
+                </>
+            )}
         </div>
     );
 };
-
 export default SelectCard;
